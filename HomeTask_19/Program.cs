2820913f86e08dc15fea057bb9569182d545e372
Kirill <ganaboss66@gmail.com>
2023-03-15 13:25:30 +0300

    Решили 19 задачку из домашки

---
+++ HomeTask_19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] d = new int[5];
+for (int i = 0; i < 5; i++)
+{
+  d[i] = n % 10;
+  n /= 10;
+}
+bool Palindrom = true;
+for (int i = 0; i < 2; i++)
+{
+  if (d[i] != d[4 - i]){
+    Palindrom = false;
+    break;
+  }
+}
+if (Palindrom)
+  Console.WriteLine("Число является палиндромом");
+else
+  Console.WriteLine("Число не является палиндромом");
